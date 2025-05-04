91a838a5b188689e3ffde7ae1b2bec5230ebf588
Varya <169041551+barbarakesel@users.noreply.github.com>
2025-05-04 23:53:32 +0300

    Create Sort.php

---
+++ algorithms/app/src/sorting/Sort.php
@@ -0,0 +1,42 @@
+<?php
+function simpleSort($arr){
+    sort($arr);
+    return($arr);
+}
+
+function bubbleSort($arr){
+    for($i = 0; $i < count($arr); $i++){
+        for($j = 0; $j < count($arr)-$i-1; $j++){
+            if($arr[$j] > $arr[$j+1]){
+                $temp = $arr[$j];
+                $arr[$j] = $arr[$j+1];
+                $arr[$j+1] = $temp;
+            }
+        }
+    }
+    return($arr);
+}
+
+function quickSort(array $arr){
+    if (count($arr) <= 1) {
+        return $arr;
+    }
+
+    $pivot = $arr[0];
+    $left = $right = [];
+
+    for ($i = 1; $i < count($arr); $i++) {
+        if ($arr[$i] < $pivot) {
+            $left[] = $arr[$i];
+        } else {
+            $right[] = $arr[$i];
+        }
+    }
+
+    return array_merge(quickSort($left), [$pivot], quickSort($right));
+}
+
+function printArray($array) {
+    echo implode(", ", $array);
+    echo "<br>";
+}
