396a3e2ef67c0ff08652ab1c03f696d2b912ca30
Varya <169041551+barbarakesel@users.noreply.github.com>
2025-05-06 10:34:21 +0300

    Update Stack.php

--- algorithms/app/src/structure/Stack.php
+++ algorithms/app/src/structure/Stack.php
@@ -1,11 +1,11 @@
 <?php
-function Stack($str)
+function stack($str)
 {
     $stack = new SplStack();
-    for($i = 0; $i < mb_strlen($str); $i++) {
+    for ($i = 0; $i < mb_strlen($str); $i++) {
         $stack->push(mb_substr($str, $i, 1));
     }
-    for($i = 0; $i < mb_strlen($str); $i++) {
+    for ($i = 0; $i < mb_strlen($str); $i++) {
         echo $stack->pop();
     }
 }
